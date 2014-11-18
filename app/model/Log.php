668ab6c93b9dd330406b167015ac969596feb47c
bkralik <bkralik@hkfree.org>
2014-11-18 17:40:30 +0100

    Logovaci funkce loguj()

--- app/model/Log.php
+++ app/model/Log.php
@@ -21,8 +21,20 @@ class Log extends Table
         return($this->findAll()->where("tabulka = ?", "uzivatel")->where("tabulka_id = ?", $uid));
     }
     
-    public function loguj($data)
+    public function loguj($tabulka, $tabulka_id, $data)
     {
-        return($this->insert($data));
+        $out = true;
+        $spolecne = array(
+            'Uzivatel_id' => $this->userService->getId(),
+            'datum' => 'NOW()',
+            'ip_adresa' => $_SERVER['REMOTE_ADDR'],
+            'tabulka' => $tabulka,
+            'tabulka_id' => $tabulka_id,
+        );
+        foreach($data as $radek)
+        {
+           $out = $out && $this->insert(array_merge($radek, $spolecne));
+        }
+        return($out);
     }
 }
