e7629a19fb976744277e96880e5cfeeeb04e6a10
bkralik <bkralik@hkfree.org>
2015-11-18 19:14:49 +0100

    Bugfix chybového hlášení u souřadnic (podrobnosti ap)

--- app/presenters/ApPresenter.php
+++ app/presenters/ApPresenter.php
@@ -109,7 +109,7 @@ class ApPresenter extends BasePresenter {
                 ->setAttribute('placeholder', '50.xxxxxx,15.xxxxxx')
                 ->setRequired('GPS souřadnice na Google mapě. Zeměpisná šířka jako reálné číslo, čárka, zeměpisná délka jako reálné číslo. Např. 50.22795,15.834133')
                 ->setOption('description', 'GPS souřadnice na Google mapě. Zeměpisná šířka jako reálné číslo, čárka, zeměpisná délka jako reálné číslo. Např. 50.22795,15.834133')
-                ->addRule(Form::PATTERN, 'Zeměpisné souřadnice prosím zadejte ve formátu 50.xxxxxx,15.xxxxxx', '^\d{2}.\d{1,8},\d{2}.\d{1,8}$');
+                ->addRule(Form::PATTERN, 'Zeměpisné souřadnice prosím zadejte ve formátu 50.xxxxxx,15.xxxxxx (bez světových stran, bez mezer, odděleno čárkou)', '^\d{2}.\d{1,8},\d{2}.\d{1,8}$');
         $form->addTextArea('poznamka', 'Poznámka', 24, 10);
         $dataIp = $this->ipAdresa;
         $typyZarizeni = $this->typZarizeni->getTypyZarizeni()->fetchPairs('id', 'text');
