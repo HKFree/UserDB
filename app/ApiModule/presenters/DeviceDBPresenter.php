09272699250976d77b3bd6a2e3f4f3979b4c2b23
evildivine <radim.drtilek@gmail.com>
2016-12-16 23:47:41 +0100

    cleaning

--- app/ApiModule/presenters/DeviceDBPresenter.php
+++ app/ApiModule/presenters/DeviceDBPresenter.php
@@ -16,21 +16,6 @@ class DeviceDBPresenter extends ApiPresenter
     public function actionDefault()
     {
         $oblastiData = $this->oblast->getSeznamOblastiBezAP();
-        // use when "Malformed UTF-8 characters, possibly incorrectly encoded" error appears inside structure,
-        // test resulting data for non-ascii chars using [^a-zA-Z\d\s:,\"\-\[\]\{\}\.\*\/\@#_\(\)] regexp
-        //$wewimoMultiData = $this->utf8ize($wewimoMultiData);
         $this->sendResponse( new JsonResponse($oblastiData) );
-    }
-
-    function utf8ize($mixed)
-    {
-        if (is_array($mixed)) {
-            foreach ($mixed as $key => $value) {
-                $mixed[$key] = $this->utf8ize($value);
-            }
-        } else if (is_string($mixed)) {
-            return utf8_encode($mixed);
-        }
-        return $mixed;
     }
 }
