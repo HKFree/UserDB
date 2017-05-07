1f2cd3e20f9363e07800d7cee9af53253afd0d7f
Pavel Kriz <pavkriz@hkfree.org>
2017-05-07 22:53:13 +0200

    typo

--- db/structures/2017-03-25-204722-latitude-longitude-uzivatele.sql
+++ db/structures/2017-03-25-204722-latitude-longitude-uzivatele.sql
@@ -1,4 +1,4 @@
-ALTER TABLE `uzivatel`
+ALTER TABLE `Uzivatel`
 ADD `location_status` ENUM('pending','valid','unknown','approx') NOT NULL DEFAULT 'pending' AFTER `email_invalid`,
 ADD `latitude` DOUBLE NULL AFTER `location_status`,
 ADD `longitude` DOUBLE NULL AFTER `latitude`;
