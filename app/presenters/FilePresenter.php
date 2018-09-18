2c4069b2aeafa9c363f4ac697cdf44acf84dae75
Radim Drtílek <radim.drtilek@atos.net>
2018-09-19 01:16:39 +0200

    finale

--- app/presenters/FilePresenter.php
+++ app/presenters/FilePresenter.php
@@ -34,24 +34,15 @@ public function actionDownloadWinboxCmd() {
     	{
             if($ip= $this->ipAdresa->getIPAdresa($this->getParam('id')))
     	    {
-                //$file = tempnam(sys_get_temp_dir(), "cmd");
-                //$handle = fopen($file, "w");
-                //fwrite($handle, "C:\winbox.exe " . $ip->ip_adresa . " " . $ip->login);
-                //fclose($handle);
                 $cmd = "C:\winbox.exe " . $ip->ip_adresa . " " . $ip->login;
-
                 $httpResponse = $this->getHttpResponse();
                 $httpResponse->setHeader('Pragma', "public");
                 $httpResponse->setHeader('Expires', 0);
                 $httpResponse->setHeader('Cache-Control', "must-revalidate, post-check=0, pre-check=0");
-                $httpResponse->setHeader('Content-Disposition', "attachment; filename=winbox.cmd");
-                $httpResponse->setHeader('Content-Type', "application/octet-stream");
+                $httpResponse->setContentType('application/octet-stream');
+                $httpResponse->setHeader('Content-Disposition', "attachment; filename=winboxrun.cmd");
                 $httpResponse->setHeader('Content-Length', strlen($cmd));
-                \Tracy\Dumper::dump(strlen($cmd));
-                \Tracy\Dumper::dump($cmd);
-                //$this->sendResponse(new TextResponse($cmd));
-                //$httpResponse->setHeader('Content-Length', filesize($file));
-                //$this->sendResponse(new FileResponse($file, "winbox.cmd", 'application/octet-stream', true));
+                $this->sendResponse(new TextResponse($cmd));
             }
         }
     }
