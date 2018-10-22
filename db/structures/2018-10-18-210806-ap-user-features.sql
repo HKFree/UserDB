a8b52db8ca99d288ea2a1ba6049aaa82e237dac7
Pavel Kriz <pavkriz@hkfree.org>
2018-10-22 22:39:14 +0200

    revert

--- db/structures/2018-10-18-210806-ap-user-features.sql
+++
@@ -1,3 +0,0 @@
-ALTER TABLE `Ap` ADD `no_auto_dns` TINYINT(1) NOT NULL DEFAULT '0' AFTER `gps`;
-ALTER TABLE `Ap` ADD `no_auto_upgrade` TINYINT(1) NOT NULL DEFAULT '0' AFTER `gps`;
-ALTER TABLE `Uzivatel`  ADD `gdpr_sledovanitv`  TINYINT(1) NOT NULL DEFAULT '0' AFTER `longitude`;
