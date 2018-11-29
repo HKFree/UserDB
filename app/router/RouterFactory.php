72b53bd74f43c556f87757da94c4b6b5a6e4cab2
Radim Drtílek <radim.drtilek@atos.net>
2018-11-29 15:04:08 +0100

    complete list of old urls

--- app/router/RouterFactory.php
+++ app/router/RouterFactory.php
@@ -22,8 +22,28 @@ class RouterFactory
 		$router = new RouteList();
 
 		$router[] = new Route($urlPrefix.'/uzivatel/list/<id>', 'UzivatelList:list', Route::ONE_WAY);
+		$router[] = new Route($urlPrefix.'/uzivatel/listall', 'UzivatelList:listall', Route::ONE_WAY);
 		$router[] = new Route($urlPrefix.'/sprava/schvalovanicc', 'SpravaCc:schvalovanicc', Route::ONE_WAY);
 		$router[] = new Route($urlPrefix.'/sprava/prehledcc', 'SpravaCc:prehledcc', Route::ONE_WAY);
+		$router[] = new Route($urlPrefix.'/sprava/usersgraph', 'SpravaGrafu:usersgraph', Route::ONE_WAY);
+		$router[] = new Route($urlPrefix.'/sprava/mailinglist', 'SpravaMailu:mailinglist', Route::ONE_WAY);
+		$router[] = new Route($urlPrefix.'/sprava/novaoblast', 'SpravaOblasti:novaoblast', Route::ONE_WAY);
+		$router[] = new Route($urlPrefix.'/sprava/noveap', 'SpravaOblasti:noveap', Route::ONE_WAY);
+		$router[] = new Route($urlPrefix.'/sprava/odchoziplatby', 'SpravaPlateb:odchoziplatby', Route::ONE_WAY);
+		$router[] = new Route($urlPrefix.'/sprava/platbycu', 'SpravaPlateb:platbycu', Route::ONE_WAY);
+		$router[] = new Route($urlPrefix.'/sprava/nesparovane', 'SpravaNesparovanych:nesparovane', Route::ONE_WAY);
+		$router[] = new Route($urlPrefix.'/sprava/prevod', 'SpravaNesparovanych:prevod', Route::ONE_WAY);
+		$router[] = new Route($urlPrefix.'/sprava/sms', 'SpravaSms:sms', Route::ONE_WAY);
+		$router[] = new Route($urlPrefix.'/sprava/ucty', 'SpravaUctu:ucty', Route::ONE_WAY);
+		$router[] = new Route($urlPrefix.'/uzivatel/sms/<id>', 'UzivatelMailSms:sms', Route::ONE_WAY);
+		$router[] = new Route($urlPrefix.'/uzivatel/email/<id>', 'UzivatelMailSms:email', Route::ONE_WAY);
+		$router[] = new Route($urlPrefix.'/uzivatel/smsall/<id>', 'UzivatelMailSms:smsall', Route::ONE_WAY);
+		$router[] = new Route($urlPrefix.'/uzivatel/emailall/<id>', 'UzivatelMailSms:emailall', Route::ONE_WAY);
+		$router[] = new Route($urlPrefix.'/uzivatel/editcc/<id>', 'UzivatelRightsCc:editcc', Route::ONE_WAY);
+		$router[] = new Route($urlPrefix.'/uzivatel/editrights/<id>', 'UzivatelRightsCc:editrights', Route::ONE_WAY);
+		$router[] = new Route($urlPrefix.'/uzivatel/account/<id>', 'UzivatelAccount:account', Route::ONE_WAY);
+		$router[] = new Route($urlPrefix.'/uzivatel/platba/<id>', 'UzivatelAccount:platba', Route::ONE_WAY);
+		
 
         $router[] = new Route($urlPrefix . '/api/<presenter>[/<action=default>[/<id>]]', [
                     'module' => 'Api'
