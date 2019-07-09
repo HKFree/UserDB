471aa851a54c9242fdea8584afe7ffb15dc50859
Vojtech Pithart <vpithart@lhota.hkfree.org>
2019-07-09 10:19:09 +0200

    Trochu lepší initial dummy data (víc lidí, IP adresy)

---
+++ db/dummy-data/2019-07-09-101010-ip-adresy.sql
@@ -0,0 +1,4 @@
+INSERT INTO `IPAdresa` (`uzivatel_id`, `ap_id`, `ip_adresa`, `dhcp`, `mac_filter`, `internet`, `smokeping`) VALUES (1, 1, '10.108.9.100', false, false, true, false);
+INSERT INTO `IPAdresa` (`uzivatel_id`, `ap_id`, `ip_adresa`, `dhcp`, `mac_filter`, `internet`, `smokeping`) VALUES (2, 1, '10.108.9.101', false, false, true, false);
+INSERT INTO `IPAdresa` (`uzivatel_id`, `ap_id`, `ip_adresa`, `dhcp`, `mac_filter`, `internet`, `smokeping`) VALUES (3, 1, '10.108.9.102', false, false, true, false);
+INSERT INTO `IPAdresa` (`uzivatel_id`, `ap_id`, `ip_adresa`, `dhcp`, `mac_filter`, `internet`, `smokeping`) VALUES (3, 1, '10.108.9.103', false, false, true, false);
