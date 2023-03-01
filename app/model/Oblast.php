c3f625921c7f79a26c5056515b8fd517711d6026
bkralik <xkralik@seznam.cz>
2023-03-01 23:20:02 +0100

    Doopravení zobrazení AP ID pro AP správce a selectbox v uzivateleditaci

--- app/model/Oblast.php
+++ app/model/Oblast.php
@@ -43,9 +43,9 @@ class Oblast extends Table
 			$apcka_oblasti = $oblast->related('Ap.Oblast_id');
 			foreach($oblast->related('Ap.Oblast_id')->order("jmeno") as $apid => $ap) {
 				if (count($apcka_oblasti) == 1) {
-                    $aps[$apid] = $ap->jmeno . ' (' . $oblast->id . ')';
+                    $aps[$apid] = $ap->jmeno . ' (' . $ap->id . ')';
                 } else {
-                    $aps[$apid] = $oblast->jmeno . ' - ' . $ap->jmeno . ' (' . $oblast->id . ')';
+                    $aps[$apid] = $oblast->jmeno . ' - ' . $ap->jmeno . ' (' . $ap->id . ')';
                 }
             }
 		}
