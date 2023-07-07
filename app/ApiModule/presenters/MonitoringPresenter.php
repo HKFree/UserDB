399f6b6f329323ed28776b3be9a59ec857e1b254
bkralik <xkralik@seznam.cz>
2023-07-07 15:16:53 +0200

    Pridana moznost omezeni monitoring API na dane AP pro dany API klic

--- app/ApiModule/presenters/MonitoringPresenter.php
+++ app/ApiModule/presenters/MonitoringPresenter.php
@@ -9,10 +9,12 @@ class MonitoringPresenter extends ApiPresenter
 {
     private $typZarizeni;
     private $ipAdresa;
+    private $ap;
 
-    function __construct(Model\TypZarizeni $typZarizeni, Model\IPAdresa $iPAdresa) {
+    function __construct(Model\TypZarizeni $typZarizeni, Model\IPAdresa $iPAdresa, Model\AP $ap) {
         $this->typZarizeni = $typZarizeni;
         $this->ipAdresa = $iPAdresa;
+        $this->ap = $ap;
     }
 
     public function actionGetTypyZarizeni()
@@ -28,17 +30,32 @@ class MonitoringPresenter extends ApiPresenter
         $this->sendResponse( new JsonResponse(['result' => 'OK', 'typyZarizeni' => $out]) );
     }
 
-    public function actionGetZarizeni($typ, $uzivatele=0) {
+    public function actionGetZarizeni($typ, $uzivatele=0, $ap=null) {
+        parent::checkApID($ap);
+
         $typZarizeni = $this->typZarizeni->find($typ);
 
         if(!$typZarizeni) {
-            $this->sendResponse( new JsonResponse(['result' => 'ERROR', 'error' => 'typZarizeni ' . $typ . " not valid"]) );
+            $this->sendResponse( new JsonResponse(['result' => 'ERROR, typZarizeni ' . $typ . ' not valid']) );
         }
 
-        $adresy = $this->ipAdresa->findAll()->where('TypZarizeni_id', $typ);
+        $adresy = $this->ipAdresa->findAll()->where('TypZarizeni_id', $typZarizeni->id);
 
         if(!$uzivatele) {
             $adresy = $adresy->where('Ap_id IS NOT NULL');
+        }
+
+        if($ap) {
+            $apRec = $this->ap->find($ap);
+            if(!$apRec) {
+                $this->sendResponse( new JsonResponse(['result' => 'ERROR, AP ID ' . $ap . ' does not exist']) );
+            }
+
+            $adresy = $adresy->where("Ap_id", $apRec->id);
+        }
+
+        if($ap && $uzivatele) {
+            $this->sendResponse( new JsonResponse(['result' => 'ERROR, selecting AP with uzivatele not implemented yet']) );
         }
 
         $out = array();
