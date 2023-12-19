8556ca605157280a1eed04c75ed5ac88e5304de1
Vojtech Pithart <vpithart@lhota.hkfree.org>
2023-12-19 22:03:52 +0100

    Chyby v init skriptu

--- db/dummy-data/2019-07-09-101010-ip-adresy.sql
+++ db/dummy-data/2019-07-09-101010-ip-adresy.sql
@@ -6,7 +6,7 @@ INSERT INTO `IPAdresa` (`uzivatel_id`, `ap_id`, `ip_adresa`, `dhcp`, `mac_filter
 INSERT INTO `IPAdresa` (`uzivatel_id`, `ap_id`, `ip_adresa`, `dhcp`, `mac_filter`, `internet`, `smokeping`) VALUES (3, null, '10.108.9.103', false, false, true, false);
 
 INSERT INTO `Subnet` (`id`, `Ap_id`, `subnet`, `gateway`, `popis`, `arp_proxy`) VALUES (1, 1, '10.108.9.0/24', '10.108.9.1', 'síť 1a-1', 0);
-INSERT INTO `Subnet` (`id`, `Ap_id`, `subnet`, `gateway`, `popis`, `arp_proxy`) VALUES (2, 1, '10.107.10.0/23', '10.108.20.0', 'síť 1a-2', 0);
+INSERT INTO `Subnet` (`id`, `Ap_id`, `subnet`, `gateway`, `popis`, `arp_proxy`) VALUES (2, 1, '10.108.10.0/23', '10.108.10.0', 'síť 1a-2', 0);
 INSERT INTO `Subnet` (`id`, `Ap_id`, `subnet`, `gateway`, `popis`, `arp_proxy`) VALUES (3, 2, '10.108.12.0/24', '10.108.12.1', 'síť 1b', 0);
 INSERT INTO `Subnet` (`id`, `Ap_id`, `subnet`, `gateway`, `popis`, `arp_proxy`) VALUES (4, 3, '10.108.13.0/24', '10.108.13.1', 'síť 2', 0);
 INSERT INTO `Subnet` (`id`, `Ap_id`, `subnet`, `gateway`, `popis`, `arp_proxy`) VALUES (5, 4, '10.108.16.0/20', '10.108.16.1', 'síť 3', 0);
