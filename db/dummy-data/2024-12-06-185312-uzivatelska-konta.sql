af11b3dd44bac9e0386f65774145ebd0af82e14f
Vojtech Pithart <vojtech.pithart@onevest.com>
2024-12-06 12:07:51 -0700

    Dummy SQL data pro UzivatelskeKonto a PrichoziPlatba

---
+++ db/dummy-data/2024-12-06-185312-uzivatelska-konta.sql
@@ -0,0 +1,18 @@
+INSERT INTO `BankovniUcet` VALUES (1,'2345678191/2010','FIO - běžný');
+INSERT INTO `BankovniUcet` VALUES (2,'2345678544/2010','FIO - spořící');
+
+INSERT INTO `UzivatelskeKonto` VALUES (1608363,29814757,1002,1,290.00,'2024-07-15','Prichozi platba',1,'2024-07-15 07:50:05','2024-07-15 07:51:06',2425057);
+INSERT INTO `UzivatelskeKonto` VALUES (1614622,NULL,1002,4,-290.00,'2024-08-01','[Automat] Clensky prispevek',1,'2024-07-31 22:02:11',NULL,NULL);
+INSERT INTO `UzivatelskeKonto` VALUES (1615117,30033228,1002,1,290.00,'2024-08-08','Prichozi platba',1,'2024-08-08 19:05:06','2024-08-08 19:06:04',2427217);
+INSERT INTO `UzivatelskeKonto` VALUES (1622990,NULL,1002,4,-290.00,'2024-09-01','[Automat] Clensky prispevek',1,'2024-08-31 22:02:01',NULL,NULL);
+INSERT INTO `UzivatelskeKonto` VALUES (1623455,30307881,1002,1,290.00,'2024-09-07','Prichozi platba',1,'2024-09-06 23:35:05','2024-09-06 23:36:06',2430931);
+INSERT INTO `UzivatelskeKonto` VALUES (1631401,NULL,1002,4,-290.00,'2024-10-01','[Automat] Clensky prispevek',1,'2024-09-30 22:02:07',NULL,NULL);
+INSERT INTO `UzivatelskeKonto` VALUES (1631860,30581481,1002,1,290.00,'2024-10-07','Prichozi platba',1,'2024-10-07 08:35:06','2024-10-07 08:36:06',2434745);
+INSERT INTO `UzivatelskeKonto` VALUES (1639791,NULL,1002,4,-290.00,'2024-11-01','[Automat] Clensky prispevek',1,'2024-10-31 23:04:48',NULL,NULL);
+INSERT INTO `UzivatelskeKonto` VALUES (1640246,30865262,1002,1,290.00,'2024-11-07','Prichozi platba',1,'2024-11-06 23:20:11','2024-11-06 23:21:07',2438523);
+
+INSERT INTO `PrichoziPlatba` VALUES (29814757,'1002','1002','2024-07-15','43-259387227/0100','Honza',290,2010,'26605485132','Členský příspěvek 1002',2,'Honza','Honza Okamžitá příchozí platba',NULL);
+INSERT INTO `PrichoziPlatba` VALUES (30033228,'1002','1002','2024-08-08','43-259387227/0100','Honza',290,2010,'26640284185','Členský příspěvek 1002',2,'Honza','Honza Okamžitá příchozí platba',NULL);
+INSERT INTO `PrichoziPlatba` VALUES (30307881,'1002','1002','2024-09-07','43-259387227/0100','Honza',290,2010,'26682333122','Členský příspěvek 1002',2,'Honza','Honza Okamžitá příchozí platba',NULL);
+INSERT INTO `PrichoziPlatba` VALUES (30581481,'1002','1002','2024-10-07','43-259387227/0100','Honza',290,2010,'26725901485','Členský příspěvek 1002',2,'Honza','Honza Okamžitá příchozí platba',NULL);
+INSERT INTO `PrichoziPlatba` VALUES (30865262,'1002','1002','2024-11-07','43-259387227/0100','Honza',290,2010,'26771387789','Členský příspěvek 1002',2,'Honza','Honza Okamžitá příchozí platba',NULL);
