2d0c3f3c9c7ea33a0855883ba77be909f336ca62
Vojtech Pithart <vojtech.pithart@onevest.com>
2024-12-06 10:21:22 -0700

    s chybou

--- www/index.php
+++ www/index.php
@@ -1,9 +1,9 @@
 <?php
 
-if (file_exists('maintenance.php')) {
+if (file_exists('maintenance.php' ) ) {
     require 'maintenance.php';
 }
 
-$container = require __DIR__ . '/../app/bootstrap.php';
+$container =  require __DIR__ . '/../app/bootstrap.php';
 
 $container->getService('application')->run();
