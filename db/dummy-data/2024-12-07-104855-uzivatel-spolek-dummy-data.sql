4ad106135f238122d92d6d4538bf68e1098d0753
bkralik <xkralik@seznam.cz>
2024-12-07 10:52:09 +0100

    Testovaci data pro rozliseni spolek / druzstvo

---
+++ db/dummy-data/2024-12-07-104855-uzivatel-spolek-dummy-data.sql
@@ -0,0 +1,2 @@
+UPDATE `Uzivatel` SET `poznamka` = 'Peta je clenem spolku i druzstva', `druzstvo` = '1' WHERE `Uzivatel`.`id` = 1001;
+UPDATE `Uzivatel` SET `poznamka` = 'Kaja je jenom clenem druzstva (ze spolku vystoupil pri konverzi)', `TypClenstvi_id` = '1', `druzstvo` = '1' WHERE `Uzivatel`.`id` = 1000;
