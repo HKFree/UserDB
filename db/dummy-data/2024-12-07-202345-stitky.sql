3438bfc74a6f2ce9fc92f80cee8b49bb38289e28
Václav Rak <vaclav.rak@gendigital.com>
2024-12-07 20:55:53 +0100

    data a struktura

---
+++ db/dummy-data/2024-12-07-202345-stitky.sql
@@ -0,0 +1,4 @@
+INSERT INTO `Stitek` (`id`, `Oblast_id`, `text`, `barva_pozadi`, `barva_popredi`) VALUES
+    (NULL, NULL, "Družstevník!!", "#000000", "#00FF00"),
+    (NULL, NULL, "učastnik", "#AA00AA", "#00FF00"),
+    (NULL, 1, "LAN123", "#00FF00", "#FFFF00");
