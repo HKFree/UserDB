6056c22f464b5ccd1659cc91c75684e3a95e7437
OndraLhota <ondrej@pithart.com>
2024-12-07 16:44:48 +0100

    implement getByUzivatelId

--- app/model/Smlouva.php
+++ app/model/Smlouva.php
@@ -6,4 +6,9 @@ use Nette;
 
 final class Smlouva extends Table {
     protected $tableName = 'Smlouva';
+
+    // Vrátí všechny smlouvy od konkretniho uživatele
+    public function getByUzivatelId(int $uzivatel_id) {
+        return $this->findAll()->where('Uzivatel_id', $uzivatel_id);
+    }
 }
