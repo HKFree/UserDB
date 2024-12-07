c4218981d16a861784deced226e520a3e7c9bb64
Matej Lang <Lang.Matej@ssakhk.cz>
2024-12-07 19:00:02 +0100

    Oprava sloupce datum nulable

---
+++ db/structures/2024-12-07-183807-oprava-smlouvy-a-podpisy-nullable.sql
@@ -0,0 +1,3 @@
+ALTER TABLE `Podpis` CHANGE `kdy_podepsano` `kdy_podepsano` TIMESTAMP NULL DEFAULT NULL ;
+ALTER TABLE `Podpis` CHANGE `kdy_odmitnuto` `kdy_odmitnuto` TIMESTAMP NULL DEFAULT NULL ;
+ALTER TABLE `Smlouva` CHANGE `kdy_ukonceno` `kdy_ukonceno` TIMESTAMP NULL DEFAULT NULL ;
