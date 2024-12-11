123b0b153907dcc8c5b222da831f76042b796de0
Vojtech Pithart <vojtech.pithart@onevest.com>
2024-12-11 05:11:45 -0700

    Refactor digisign_generovat_ucastnickou_smlouvu do App/Console

--- app/presenters/UzivatelActionsPresenter.php
+++ app/presenters/UzivatelActionsPresenter.php
@@ -129,11 +129,15 @@ class UzivatelActionsPresenter extends UzivatelPresenter
 
     public function actionHandleSubscriberContractPreview() {
         $user_id = $this->getParameter('id');
+        $uzivatel =  $this->uzivatel->find($user_id);
 
         $tmpname = sprintf('/dev/shm/document_%u.pdf', rand(1, 1e9));
-        exec(__DIR__."/../../bin/nahled-ucastnicke-smlouvy.php {$user_id} > $tmpname", );
+        $pdfData = \App\Services\GeneratorSmlouvy::nahledUcastnickeSmlouvy($uzivatel);
+        file_put_contents($tmpname, $pdfData);
 
-        $this->presenter->sendResponse(new Nette\Application\Responses\FileResponse($tmpname, "náhled smlouvy", 'application/pdf', false));
+        $this->sendResponse(new Nette\Application\Responses\FileResponse($tmpname, "náhled smlouvy", 'application/pdf', false));
+
+        unlink($tmpname);
     }
 
     public function actionHandleSubscriberContract() {
