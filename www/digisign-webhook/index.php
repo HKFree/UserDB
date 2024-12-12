7456ff20fe832ca3fdd06fbf7a5dd9316badb93b
Vojtech Pithart <vojtech.pithart@onevest.com>
2024-12-12 11:54:28 -0700

    Stahovat smlouvu vcetne auditni stopy; hook "obálka odeslána"

--- www/digisign-webhook/index.php
+++ www/digisign-webhook/index.php
@@ -20,6 +20,7 @@ function print_and_log($message) {
 
 $body = file_get_contents('php://input');
 // $body = '{"id":"0193b88b-67d0-70c2-a4e6-fed356998bac","event":"envelopeCompleted","name":"envelope.completed","time":"2024-12-12T02:46:04+01:00","entityName":"envelope","entityId":"0193b88a-e045-70c7-ac0f-e6adc93009c3","data":{"status":"completed"},"envelope":{"id":"0193b88a-e045-70c7-ac0f-e6adc93009c3","status":"completed"}}';
+// $body = '{"id":"0193b7bc-3bee-7393-b19d-02c49b5781a9","event":"envelopeSent","name":"envelope.sent","time":"2024-12-11T22:59:46+01:00","entityName":"envelope","entityId":"0193b88a-e045-70c7-ac0f-e6adc93009c3","data":{"status":"sent"},"envelope":{"id":"0193b7bc-1797-70f6-a095-231c750d3487","status":"sent"}}';
 
 if (strlen($body) < 10000) {
     error_log("digisign_webhook: payload: " . print_r($body, true));
@@ -45,79 +46,105 @@ $dgs = new DigiSign([
 ]);
 $ENVELOPES = $dgs->envelopes();
 
+if ($hook->entityName != "envelope") {
+    return;
+}
+
+$smlouva = $SmlouvaModel->findOneBy(['externi_id' => $hook->entityId]);
+if (!$smlouva) {
+    print_and_log("Smlouvu nemame v DB -> ignorovat");
+    return;
+}
+
+if ($smlouva->typ != 'ucastnicka') {
+    print_and_log(sprintf("Smlouva #%u typ %s tady neumime zpracovat", $smlouva->id, $smlouva->typ));
+    return;
+}
+
+// zpetne volat API nacist detaily smlouvy:
+$envelope = $ENVELOPES->get($hook->entityId);
+
+if (!$smlouva) {
+    print_and_log("Smlouvu nezna digisign API -> ignorovat");
+    return;
+}
+
 switch ($hook->event) {
     case 'envelopeSent': // obálka odeslána
         /**
          *  {"id":"0193b7bc-3bee-7393-b19d-02c49b5781a9","event":"envelopeSent","name":"envelope.sent","time":"2024-12-11T22:59:46+01:00","entityName":"envelope","entityId":"0193b7bc-1797-70f6-a095-231c750d3487","data":{"status":"sent"},"envelope":{"id":"0193b7bc-1797-70f6-a095-231c750d3487","status":"sent"}}
          */
+        $odeslano_kdy = $envelope->recipients[0]->sentAt;
+        $odeslano_kam = $envelope->recipients[0]->email;
+
+        // 1. uložit do DB že je smlouva odeslaná
+        $novaPoznamka = sprintf(
+            "%s%sSmlouva odeslána  %s na adresu %s.",
+            $smlouva->poznamka,
+            empty($smlouva->poznamka) ? '' : "\n",
+            $odeslano_kdy->format('d.m.Y H:i'),
+            $odeslano_kam
+        );
+        $smlouva->update(['poznamka' => $novaPoznamka]);
 
         break;
     case 'envelopeCompleted': // obálka dokončena (podepsána všemi podepisujícími)
         /**
          * {"id":"0193b7d7-4941-7217-ad91-f2cf0ca9a8e6","event":"envelopeCompleted","name":"envelope.completed","time":"2024-12-11T23:29:19+01:00","entityName":"envelope","entityId":"0193b1df-239d-7260-b479-111da9ddfbac","data":{"status":"completed"},"envelope":{"id":"0193b1df-239d-7260-b479-111da9ddfbac","status":"completed"}}
          */
-        $smlouva = $SmlouvaModel->findOneBy(['externi_id' => $hook->entityId]);
-        if (!$smlouva) {
-            print_and_log("nemame takovou smlouvu -> ignorovat");
-            break;
-        }
-        // zpetne volat API a overit stav:
-        $envelope = $ENVELOPES->get($hook->entityId);
+
+        // ověřit skutečný stav
         if ($envelope->status !== 'completed') {
             print_and_log("Nesedi envelope status ({$envelope->status})");
             break;
         }
-        if ($smlouva->typ == 'ucastnicka') {
-            // 1. uložit do DB že je smlouva podepsaná
-            $podpis = $PodpisSmlouvyModel->findOneBy(['smlouva_id' => $smlouva->id, 'smluvni_strana' => 'ucastnik']);
-            $podpis->update(['kdy_podepsano' => $hook->time]);
-            print_and_log(sprintf("smlouva #%u podpis ucastnika \"%s\" datum/cas: %s", $smlouva->id, $podpis->jmeno, $hook->time));
-            // 2. stáhnout podepsaný PDF a uložit
-            $doc1id = $envelope->documents[0]->id;
-            $fileResponse = $ENVELOPES->documents($envelope)->download($doc1id);
-            $documentFullName = "{$FILE_STORAGE_PATH}/{$envelope->documents[0]->name}";
-            $fileResponse->save($documentFullName);
-            print_and_log(sprintf('Ulozeno do %s', $documentFullName));
-            $smlouva->update(['podepsany_dokument_nazev' => $envelope->documents[0]->name]);
-            $smlouva->update(['podepsany_dokument_content_type' => 'application/pdf']);
-            $smlouva->update(['podepsany_dokument_path' => $documentFullName]);
-            // 3. pokud nemáme datum narození, zkusíme parsovat vyplněný ze smlouvy
-            $uzivatel = $UzivatelModel->find($smlouva->uzivatel);
-            if (!$uzivatel->datum_narozeni) {
-                $recipient1 = $ENVELOPES->recipients($envelope)->get($envelope->recipients[0]->id);
-                $tags = $recipient1->tags->toArray();
-                foreach ($tags as $tag) {
-                    if ($tag['recipientClaim'] == 'birthdate') {
-                        $birthdate_dirty = $tag['value'];
-                        try {
-                            $d = new DateTime(preg_replace('/\s+/', '', $birthdate_dirty) . " 00:00:00");
-                            print_and_log(sprintf('datum narozeni [%s] parsovano jako %s', $birthdate_dirty, $d->format('Y-m-d')));
-                            $uzivatel->update(['datum_narozeni' => $d->format('Y-m-d')]);
-                        } catch (Exception $e) {
-                            print_and_log(sprintf('datum narozeni [%s] neni jasny -> zapsat do poznamky', $birthdate_dirty));
-                            $novaPoznamka = sprintf(
-                                "%s%sNejasné datum narození [%s] (z digisign smlouvy #%u ze dne %s)",
-                                $uzivatel->poznamka,
-                                empty($uzivatel->poznamka) ? '' : "\n\n",
-                                $birthdate_dirty,
-                                $smlouva->id,
-                                $podpis->kdy_podepsano->format("d.m.Y")
-                            );
-                            $uzivatel->update(['poznamka' => $novaPoznamka]);
-                        }
+
+        // 1. uložit do DB že je smlouva podepsaná
+        $podpis = $PodpisSmlouvyModel->findOneBy(['smlouva_id' => $smlouva->id, 'smluvni_strana' => 'ucastnik']);
+        $podpis->update(['kdy_podepsano' => $hook->time]);
+        print_and_log(sprintf("smlouva #%u podpis ucastnika \"%s\" datum/cas: %s", $smlouva->id, $podpis->jmeno, $hook->time));
+        // 2. stáhnout podepsaný PDF a uložit
+        $fileResponse = $ENVELOPES->download($envelope->id);
+        $documentFullName = "{$FILE_STORAGE_PATH}/{$envelope->documents[0]->name}";
+        $fileResponse->save($documentFullName);
+        print_and_log(sprintf('Ulozeno do %s', $documentFullName));
+        $smlouva->update(['podepsany_dokument_nazev' => $envelope->documents[0]->name]);
+        $smlouva->update(['podepsany_dokument_content_type' => 'application/pdf']);
+        $smlouva->update(['podepsany_dokument_path' => $documentFullName]);
+        // 3. pokud nemáme datum narození, zkusíme parsovat vyplněný ze smlouvy
+        $uzivatel = $UzivatelModel->find($smlouva->uzivatel);
+        if (!$uzivatel->datum_narozeni) {
+            $recipient1 = $ENVELOPES->recipients($envelope)->get($envelope->recipients[0]->id);
+            $tags = $recipient1->tags->toArray();
+            foreach ($tags as $tag) {
+                if ($tag['recipientClaim'] == 'birthdate') {
+                    $birthdate_dirty = $tag['value'];
+                    try {
+                        $d = new DateTime(preg_replace('/\s+/', '', $birthdate_dirty) . " 00:00:00");
+                        print_and_log(sprintf('datum narozeni [%s] parsovano jako %s', $birthdate_dirty, $d->format('Y-m-d')));
+                        $uzivatel->update(['datum_narozeni' => $d->format('Y-m-d')]);
+                    } catch (Exception $e) {
+                        print_and_log(sprintf('datum narozeni [%s] neni jasny -> zapsat do poznamky', $birthdate_dirty));
+                        $novaPoznamka = sprintf(
+                            "%s%sNejasné datum narození [%s] (z digisign smlouvy #%u ze dne %s)",
+                            $uzivatel->poznamka,
+                            empty($uzivatel->poznamka) ? '' : "\n\n",
+                            $birthdate_dirty,
+                            $smlouva->id,
+                            $podpis->kdy_podepsano->format("d.m.Y")
+                        );
+                        $uzivatel->update(['poznamka' => $novaPoznamka]);
                     }
                 }
-
             }
-            // 4. zrušit členství ve spolku (pokud existuje)
-            if ($uzivatel->spolek) {
-                $uzivatel->update(['TypClenstvi_id' => 1]); // zrušeno
-            }
-            // 5. nastavit "vztah" s družstvem
-            $uzivatel->update(['druzstvo' => 1]);
-        } else {
-            print_and_log(sprintf("smlouva #%u typ %s tady neumime zpracovat", $smlouva->id, $smlouva->typ));
         }
+        // 4. zrušit členství ve spolku (pokud existuje)
+        if ($uzivatel->spolek) {
+            $uzivatel->update(['TypClenstvi_id' => 1]); // zrušeno
+        }
+        // 5. nastavit "vztah" s družstvem
+        $uzivatel->update(['druzstvo' => 1]);
+
         break;
     case 'envelopeExpired': // obálka expirovala
 
