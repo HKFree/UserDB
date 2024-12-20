19166c1d1f16ad0763e6fcd5d45f8a92fbd7944b
lubikx <nosek@netium.cz>
2024-12-20 23:45:41 +0100

    Implementace MexSMSSenderu (#142)
    * Začátek implementace posílání SMSek přes nové Materna API
    
    * Doplneni default local configu o konfiguraci MexSMS
    
    * Začátek implementace posílání SMSek přes nové Materna API
    
    * Doplneni default local configu o konfiguraci MexSMS
    
    * Fix posilani SMS z console
    
    * Fix formatovani souboru SendSMSCommand
    
    * Migrace na MexSMSSender
    
    * Fix formatovani
    
    * Materna MEX - použít alfa Id "hkfree.org"
    
    ---------
    
    Co-authored-by: bkralik <xkralik@seznam.cz>
    Co-authored-by: Vojtech Pithart <vojtech.pithart@onevest.com>

---
+++ app/model/Komunikace.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Model;
+
+use Nette;
+use App\Services\MexSmsSender;
+use App\Model\Uzivatel;
+
+/**
+ * @author bkralik
+ */
+class Komunikace extends Table
+{
+    public $smsSender;
+    public $uzivatel;
+
+    /**
+     * @var string
+     */
+    protected $tableName = 'Komunikace';
+
+    public function __construct(MexSmsSender $m, Uzivatel $u) {
+        $this->smsSender = $m;
+        $this->uzivatel = $u;
+    }
+
+    public function posliSMS(array $uzivateleID, string $zprava) {
+        $cisla = [];
+        foreach ($uzivateleID as $uid) {
+            $u = $this->uzivatel->getUzivatel($uid);
+            if ($this->smsSender->checkCzechNumber($u->telefon)) {
+                $cisla[] = $u->telefon;
+            }
+        }
+
+        $this->smsSender->sendSMS($cisla, $zprava);
+    }
+}
---
+++ app/services/MexSmsSender.php
@@ -0,0 +1,133 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Services;
+
+use GuzzleHttp\Client;
+use Exception;
+use Nette\Caching\Cache;
+
+/**
+ * Class MaxSmsSender
+ * @author bkralik <bkralik@hkfree.org>
+ */
+class MexSmsSender
+{
+    /** @var string */
+    private $mexUrl;
+
+    /** @var string */
+    private $mexUser;
+
+    /** @var string */
+    private $mexPass;
+
+    /** @var Cache */
+    private $cache;
+
+    public function __construct(string $mexUrl, string $mexUser, string $mexPass, Cache $cache) {
+        $this->mexUrl = $mexUrl;
+        $this->mexUser = $mexUser;
+        $this->mexPass = $mexPass;
+        $this->cache = $cache;
+    }
+
+    public function checkCzechNumber(string $number): bool {
+        return (strlen($this->formatCzechNumber($number, false)) > 0);
+    }
+
+    private function formatCzechNumber(string $number, bool $strict = true): string {
+        $number_cleared = preg_replace('/\s/', '', $number);
+
+        // match 777222555
+        if (preg_match('/^\d{9}$/', $number_cleared)) {
+            return ("00420" . $number);
+        }
+
+        // match +420777222555
+        if (preg_match('/^\+420(\d{9})$/', $number_cleared, $matches)) {
+            return ("00420" . $matches[1]);
+        }
+
+        // match 420777222555 (not really valid)
+        if (preg_match('/^420(\d{9})$/', $number_cleared, $matches)) {
+            return ("00420" . $matches[1]);
+        }
+
+        if ($strict) {
+            throw new SmsSenderException("Zadané číslo (" . $number . ") neni validní pro odeslání SMS.");
+        } else {
+            return ("");
+        }
+    }
+
+    private function apiCall(string $endpoint, array $data, string $method = "POST", bool $initial = false) {
+        $client = new Client();
+
+        try {
+            $reqdata = [
+                'json' => $data,
+                'debug' => false
+            ];
+            if (!$initial) {
+                $reqdata['headers'] = ['Authorization' => "Bearer " . $this->getToken()];
+            }
+
+            $r = $client->request($method, $this->mexUrl . $endpoint, $reqdata);
+
+        } catch (\GuzzleHttp\Exception\TransferException $e) {
+            throw new SmsSenderException("Chyba přístupu k MEX API. Kontaktujte správce UserDB.\n" . $e->getMessage());
+        }
+
+        if ($r->getStatusCode() == 401) {
+            throw new SmsSenderException("Chyba přihlášení k MEX API (chybný token). Kontaktujte správce UserDB.");
+        }
+
+        if ($r->getStatusCode() != 200) {
+            throw new SmsSenderException("Chyba vstupu MEX API. Kontaktujte správce UserDB.");
+        }
+
+        $rBody = (string) $r->getBody();
+        return (json_decode($rBody, true));
+    }
+
+    private function login() {
+        $data = [
+            "username" => $this->mexUser,
+            "password" => $this->mexPass
+        ];
+
+        $res = $this->apiCall("/login", $data, "POST", true);
+
+        return ($res["token"]);
+    }
+
+    private function getToken() {
+        return ($this->cache->load("authToken", function (&$dependencies) {
+            $dependencies[Cache::EXPIRE] = '50 minutes';
+            return ($this->login());
+        }));
+    }
+
+    public function sendSMS(array $bnumbers, string $text) {
+        $data = array();
+        foreach ($bnumbers as $bnumber) {
+            $data[] = [
+                "textId" => 'hkfree.org',
+                "bNumber" => $this->formatCzechNumber($bnumber),
+                "messageType" => "SMS",
+                "text" => $text,
+            ];
+        }
+
+        $res = $this->apiCall("/sms/send", $data, "POST");
+
+        // TODO: Osetrit chybu pri odeslani SMS
+    }
+
+}
+
+class SmsSenderException extends Exception
+{
+}
