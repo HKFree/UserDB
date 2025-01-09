a88208241dac663252b2209f174237ede2ac20e6
bkralik <xkralik@seznam.cz>
2025-01-09 23:10:20 +0100

    Implementace API pro posilani SMS pres UserDB

---
+++ app/ApiModule/presenters/KomunikacePresenter.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\ApiModule\Presenters;
+
+use Exception;
+use Nette\Application\Responses\JsonResponse;
+
+class KomunikacePresenter extends ApiPresenter
+{
+    private $komunikace;
+    private $uzivatel;
+
+    public function __construct(\App\Model\Komunikace $komunikace, \App\Model\Uzivatel $uzivatel) {
+        $this->komunikace = $komunikace;
+        $this->uzivatel = $uzivatel;
+    }
+
+    public function renderDefault() {
+        $this->sendResponse(new JsonResponse(['result' => 'Method not implemented']));
+    }
+
+    public function actionSendSMS() {
+        parent::forceMethod("POST");
+
+        $uid = $this->getHttpRequest()->getPost("uid", null);
+        $text = $this->getHttpRequest()->getPost("text", null);
+
+        $u = $this->uzivatel->getUzivatel($uid);
+        if (!$u) {
+            $this->sendResponse(new JsonResponse(['result' => 'User ID not found', 'serverTime' => date("c")]));
+        }
+
+        parent::checkApID($u->Ap->id);
+
+        try {
+            $this->komunikace->posliSMS([$u], $text);
+        } catch (Exception $e) {
+            $this->sendResponse(new JsonResponse(['result' => 'SMS send failed', 'reason' => strval($e), 'serverTime' => date("c")]));
+        }
+
+        $this->sendResponse(new JsonResponse(['result' => 'OK']));
+    }
+}
