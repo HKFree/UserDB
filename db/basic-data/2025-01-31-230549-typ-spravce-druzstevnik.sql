fce9d430fbc02d3e7855b040855bc1dfc90272ce
bkralik <xkralik@seznam.cz>
2025-01-31 23:10:14 +0100

    Doplneni indexu na log, vytvoreni pohledu cc_druzstvo, doplneni ciselniku typSpravce

---
+++ db/basic-data/2025-01-31-230549-typ-spravce-druzstevnik.sql
@@ -0,0 +1,6 @@
+INSERT IGNORE INTO `TypSpravceOblasti` (`id`, `text`) VALUES(5, 'PMVTEAM');
+INSERT IGNORE INTO `TypSpravceOblasti` (`id`, `text`) VALUES(6, 'KONTROLA');
+INSERT IGNORE INTO `TypSpravceOblasti` (`id`, `text`) VALUES(7, 'EXTSUPPORT');
+INSERT IGNORE INTO `TypSpravceOblasti` (`id`, `text`) VALUES(8, 'SKLADNIK');
+INSERT IGNORE INTO `TypSpravceOblasti` (`id`, `text`) VALUES(9, 'PŘEDSTAVENSTVO');
+INSERT IGNORE INTO `TypSpravceOblasti` (`id`, `text`) VALUES(10, 'DRUŽSTEVNÍK');
---
+++ db/structures/2025-01-31-230234-pridani-indexu-na-log.sql
@@ -0,0 +1,2 @@
+CREATE INDEX idx_log_tabulka_tabulka_id ON Log(tabulka, tabulka_id);
+CREATE INDEX idx_log_sloupec_datum ON Log(sloupec, datum);
