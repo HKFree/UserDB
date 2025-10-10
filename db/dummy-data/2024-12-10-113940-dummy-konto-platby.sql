ad9ed4b7295e47f216cb11209f8fa09fc23ae41a
pavkriz <pavel.kriz.cz@gmail.com>
2025-10-10 15:50:00 +0200

    Dummy data updated and nextras/migrations config fixed

--- db/dummy-data/2024-12-10-113940-dummy-konto-platby.sql
+++ db/dummy-data/2024-12-10-113940-dummy-konto-platby.sql
@@ -1,7 +1,8 @@
-INSERT INTO `UzivatelskeKonto` (`spolek`, `druzstvo`, `Uzivatel_id`, `TypPohybuNaUctu_id`, `castka`, `datum`, `poznamka`, `zmenu_provedl`) VALUES
-('0', '1', 1001, 1, 290.00, '2024-08-08', 'Prichozi platba', 1),
-('0', '1', 1001, 4, -290.00, '2024-09-01', '[Automat] Clensky prispevek', 1),
-('0', '1', 1001, 1, 290.00, '2024-11-11', 'Prichozi platba', 1),
-('0', '1', 1001, 4, -290.00, '2024-12-01', '[Automat] Clensky prispevek', 1),
-('1', '0', 1001, 1, 290.00, '2024-11-11', 'Prichozi platba', 1),
-('1', '0', 1001, 4, -290.00, '2024-12-01', '[Automat] Clensky prispevek', 1);
+INSERT INTO `UzivatelskeKonto` (`spolek`, `druzstvo`, `Uzivatel_id`, `TypPohybuNaUctu_id`, `castka`, `datum`, `poznamka`, `zmenu_provedl`, `cislo_dokladu`, `datum_odeslani_dokladu`) VALUES
+('0', '1', 1001, 1, 290.00, '2024-08-08', 'Prichozi platba', 1, 2400001, '2025-08-08 08:49:22'),
+('0', '1', 1001, 4, -290.00, '2024-09-01', '[Automat] Clensky prispevek', 1, null, null),
+('0', '1', 1001, 1, 290.00, '2024-11-11', 'Prichozi platba', 1, 2400002, '2025-11-11 11:49:22'),
+('0', '1', 1001, 4, -290.00, '2024-12-01', '[Automat] Clensky prispevek', 1, null, null),
+('1', '0', 1001, 1, 290.00, '2024-11-11', 'Prichozi platba', 1, 2400003, '2025-11-11 12:49:22'),
+('1', '0', 1001, 4, -290.00, '2024-12-01', '[Automat] Clensky prispevek', 1, null, null),
+('0', '1', 1001, 1, 290.00, '2025-04-11', 'Prichozi platba', 1, 2500001, null);
